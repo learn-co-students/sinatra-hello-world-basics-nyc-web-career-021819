9a18d38f3d32a75873afe404b9ada80d4de2cd18
rrosztoczy <rrosztoczy@gmail.com>
2019-03-11 09:42:05 -0400

    Finished

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
+require 'Sinatra'
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+
+
+    get "/" do 
+    status = 200
+    "Hello, World!"
+    end
+    
+
+
 
 
 end
